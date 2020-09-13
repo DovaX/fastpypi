d48c5319ad18001953f86c7c107bb174f488909c
Dominik Vach <vach.dominik@gmail.com>
2020-09-13 23:52:24 +0200

    create licence command

--- easypypi_core.py
+++ easypypi_core.py
@@ -37,6 +37,36 @@ def create_setup_py():
     with open("test.py","w+") as file:
         file.write(setup_py_str)
 
+def create_licence():
+    
+    author=entry3.text.get()
+    
+    mit_licence_str="""MIT License
+        
+        Copyright (c) 2020 """+author+"""
+        
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+        
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+        
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+    """
+    
+    with open("test.py","w+") as file:
+        file.write(mit_licence_str)
+
 
 def upload():
     pass
@@ -49,6 +79,7 @@ label2=dg.Label(gui1.window,"Version",2,1)
 entry2=dg.Entry(gui1.window,2,2,"0.0.1")
 
 btn1=dg.Button(gui1.window,"Create setup.py file",create_setup_py,2,3)
+btn1=dg.Button(gui1.window,"Create licence file",create_setup_py,3,3)
 
 label3=dg.Label(gui1.window,"Author",3,1)
 entry3=dg.Entry(gui1.window,3,2)
