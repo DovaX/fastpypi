cdaf93a5506a084d9faa5376777dbdff4f36af4b
Dominik Vach <vach.dominik@gmail.com>
2020-12-30 21:34:43 +0100

    add loading of setup.py, change of structure

--- easypypi_core.py
+++ easypypi_core.py
@@ -1,6 +1,37 @@
 import os
-import dogui.dogui_core as dg
+import dogui.dogui.dogui_core as dg
 from keepvariable.keepvariable_core import Var,kept_variables,save_variables,load_variable
+
+
+def load_setup_py():
+    """Checks whether there is already a setup file for given package"""
+    with open("setup.py","r") as file:
+        rows=file.readlines()
+        text_rows="\n".join(rows)
+                
+        name=text_rows.split("name")[1].split(",")[0].replace("'","").replace("=","").replace('"','')       
+        version=text_rows.split("version")[1].split(",")[0].replace("'","").replace("=","").replace('"','') 
+        author_email=text_rows.split("author_email")[1].split(",")[0].replace("'","").replace("=","").replace('"','')       
+        url=text_rows.split("url")[1].split(",")[0].replace("'","").replace("=","").replace('"','')       
+        author=text_rows.split("author")[1].split(",")[0].replace("'","").replace("=","").replace('"','')       
+        short_description=text_rows.split("    description")[1].split(",")[0].replace("'","").replace("=","").replace('"','') #its on new line
+        try:
+            dependencies=text_rows.split("install_requires=[")[1].split("]")[0].replace("'","").replace('"','').replace(" ","").replace("\n","")   
+            dependencies="'"+"','".join(dependencies.split(","))+"'"
+        except: #if not found, fill nothing
+            dependencies=""
+        
+        
+        entry1.text.set(name)
+        entry2.text.set(version)
+        entry3.text.set(author)
+        entry4.text.set(author_email)
+        entry5.text.set(url)
+        entry8.text.set(short_description)
+        entry7.text.set(dependencies)
+        
+
+
 
 def create_setup_py():
     
@@ -102,40 +133,54 @@ def upload_to_pypi():
 def change_directory():
     new_dir=entry6.text.get()
     os.chdir(new_dir)
+    new_dir=label21.text.set(new_dir)
+    
     print(os.getcwd())
     
 
 current_dir=os.getcwd()
 gui1=dg.GUI("Easy PyPI Tool")
 
-label1=dg.Label(gui1.window,"Package name",1,1)
-entry1=dg.Entry(gui1.window,1,2)
-label2=dg.Label(gui1.window,"Version",2,1)
-entry2=dg.Entry(gui1.window,2,2,"0.0.1")
+label1=dg.Label(gui1.window,"Package name",3,1)
+entry1=dg.Entry(gui1.window,3,2)
+label2=dg.Label(gui1.window,"Version",4,1)
+entry2=dg.Entry(gui1.window,4,2,"0.0.1")
  
-btn1=dg.Button(gui1.window,"Create setup.py file",create_setup_py,2,3)
-btn2=dg.Button(gui1.window,"Create licence file",create_licence,3,3)
-btn3=dg.Button(gui1.window,"Change directory",change_directory,2,4)
-btn4=dg.Button(gui1.window,"Create package distribution",create_package,3,4)
-btn5=dg.Button(gui1.window,"Upload package to PyPI",upload_to_pypi,4,4)
-btn6=dg.Button(gui1.window,"Create init file",create_init_file,5,4)
+btn1=dg.Button(gui1.window,"Create setup.py file",create_setup_py,5,4)
+btn2=dg.Button(gui1.window,"Create MIT Licence file",create_licence,5,3)
+btn6=dg.Button(gui1.window,"Create __init__.py file",create_init_file,4,3)
+btn8=dg.Button(gui1.window,"Create README.md file",create_readme,4,4)
 btn7=dg.Button(gui1.window,"Move python files to package folder",move_files_to_package_folder,6,4)
-btn8=dg.Button(gui1.window,"Create Readme",create_readme,7,4)
+
+
+btn4=dg.Button(gui1.window,"Create package distribution",create_package,8,4)
+btn5=dg.Button(gui1.window,"Upload package to PyPI",upload_to_pypi,9,4)
 
 
 
-
-label3=dg.Label(gui1.window,"Author",3,1)
-entry3=dg.Entry(gui1.window,3,2)
-label4=dg.Label(gui1.window,"Author email",4,1)
-entry4=dg.Entry(gui1.window,4,2,width=50)
-label5=dg.Label(gui1.window,"Url",5,1)
-entry5=dg.Entry(gui1.window,5,2,width=50)
-label6=dg.Label(gui1.window,"Current dir",6,1)
-entry6=dg.Entry(gui1.window,6,2,text_input=current_dir,width=50)
-label7=dg.Label(gui1.window,"Dependencies",7,1)
-entry7=dg.Entry(gui1.window,7,2,width=50)
+label3=dg.Label(gui1.window,"Author",5,1)
+entry3=dg.Entry(gui1.window,5,2)
+label4=dg.Label(gui1.window,"Author email",6,1)
+entry4=dg.Entry(gui1.window,6,2,width=50)
+label5=dg.Label(gui1.window,"Url",7,1)
+entry5=dg.Entry(gui1.window,7,2,width=50)
 label8=dg.Label(gui1.window,"Short Description",8,1)
 entry8=dg.Entry(gui1.window,8,2,width=50)
+label7=dg.Label(gui1.window,"Dependencies",9,1)
+entry7=dg.Entry(gui1.window,9,2,width=50)
+
+
+
+
+
+label6=dg.Label(gui1.window,"Current directory:",1,1)
+
+label21=dg.Label(gui1.window,current_dir,1,2)
+
+label20=dg.Label(gui1.window,"Change directory to:",2,1)
+entry6=dg.Entry(gui1.window,2,2,text_input=current_dir,width=50)
+btn3=dg.Button(gui1.window,"Change directory",change_directory,2,3)
+btn9=dg.Button(gui1.window,"Load setup.py file",load_setup_py,2,4)
+
 
 gui1.build_gui()
